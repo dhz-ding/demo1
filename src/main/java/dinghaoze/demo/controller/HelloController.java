3a24b453c2e98c70ce397d249bacad6aeaf08675
dinghaoze <18438100254@163.com>
2020-05-21 14:00:09 +0800

    Initial commit

---
+++ src/main/java/dinghaoze/demo/controller/HelloController.java
@@ -0,0 +1,46 @@
+package dinghaoze.demo.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * @Author: dhz
+ * @Date: 2020/5/20 17:15
+ */
+
+@RestController
+public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String sayHello(HttpServletRequest request) {
+        logger.info("本次访问ip为："+getIpAddress(request));
+        return "hello world";
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
---
+++ src/main/java/dinghaoze/demo/service/HelloService.java
@@ -0,0 +1,13 @@
+package dinghaoze.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: dhz
+ * @Date: 2020/5/20 17:53
+ */
+
+@Service
+public class HelloService {
+
+}
